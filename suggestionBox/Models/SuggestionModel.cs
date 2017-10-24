4dc74b3db3abd57172363682d3cb65423423b614
PensCup87 <bdt9123@gmail.com>
2017-10-24 13:22:11 -0400

    Program has many issues and did not run. Jarryd and Daniel could not fix

---
+++ suggestionBox/Models/SuggestionModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace suggestionBox.Models
+{
+    public class SuggestionModel
+    {
+        //Properties - fields needed for model - only using as a template, not an object/class
+        //[Key] is a data annotation that allows the next variable to be specified  as uniquie to the row 
+        //have to use show potential fixes to make key work
+        [Key]
+        public int ID { get; set; }
+        public string Topic { get; set; }
+        public string Comment { get; set; }
+
+        //anytime there is a change to the model, build the model to check for success(CTRL + SHIFT + B)
+
+    }
+}
